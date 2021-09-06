700c4ee3223e10fe4be394db91fe3e67cca8d965
Kingster <kingsterrck@126.com>
2021-09-06 12:18:43 +0800

    September 6

--- View/Scratchy htmls/js/sign_up.js
+++ View/Scratchy htmls/js/sign_up.js
@@ -1,27 +1,45 @@
 //get client's window height and width
 const window_height = window.innerHeight;
 const window_width = window.innerWidth;
-var UIList = document.getElementsByClassName("SignUpUI");
+const UIList = document.getElementsByClassName("signUpUI");
+var UICounter = 1;
+var signUpFirstStep = document.getElementById("signUpFirstStep");
+var signUpSecondStep = document.getElementById("signUpSuccess");
 
 defaultSetup()
 function defaultSetup(){
     //document.getElementById("mainBox").style.height = window_height - 70 + "px"
-    document.getElementById("signUpFirstStep").style.width = 0.18 * window_width + "px";
-    const signUpFirstWidth = document.getElementById("signUpFirstStep").style.width;
+    signUpFirstStep.style.width = 0.18 * window_width + "px";
+    signUpSecondStep.style.width = 0.23 * window_width + "px";
+    const signUpFirstWidth = signUpFirstStep.style.width;
     let signUpBoxList = document.getElementsByClassName("loginBox");
+    let shortSignUpBoxList = document.getElementsByClassName("shortLoginBox");
     for (i=0;i<signUpBoxList.length;i++) {
         signUpBoxList[i].style.width=signUpFirstWidth;   // set the width of things in the login box
     }
-    document.getElementById("signUpFirstStep").style.width = 0.23 * window_width + "px";
-    for (i=0;i<UIList.length;i++) {
+    for (i=0;i<shortSignUpBoxList.length;i++) {
+        shortSignUpBoxList[i].style.width = 0.09 * window_width + "px";
+    }
+    signUpFirstStep.style.width = 0.23 * window_width + "px";
+    for (i=0;i<UIList.length;i++) { // let all the UI blocks be invisible
         UIList[i].style.display="none";
     }
-    document.getElementById("SignUpFirstStep").style.display="block";
+    signUpFirstStep.style.display="block";
+}
+
+function changeUI() {
+    for (i=0;i<UIList.length;i++) {
+        UIList[i].style.display = "none";
+        if (UIList[i].getAttribute("data")==UICounter) {
+            UIList[i].style.display= "block";
+            UICounter++;
+        }
+    }
 }
 
 function changeToSecond() {
     for (i=0;i<UIList.length;i++) {
         UIList[i].style.display = "none";
     }
-    document.getElementById("signUpSecondStep").style.display="block";
+    document.getElementById("signUpSuccess").style.display="block";
 }
